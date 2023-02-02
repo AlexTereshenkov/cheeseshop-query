6dd5ef5ba533eea8181864be9ab406613ca2cbd9
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2023-02-02 20:37:09 +0000

    pants: add experimental_shell_command to use in a test

--- tests/pantstests/test_commands.py
+++ tests/pantstests/test_commands.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-import pytest
 
-
-@pytest.mark.vcr()
 def test_generate_test_data_with_pants_shell_command():
-    """Test running a test that is known to cause generation of a file via a
-    shell command."""
+    """Test running a test that is known to cause generation of a file.
+
+    The file is generated via a shell command, see
+    https://www.pantsbuild.org/docs/run-shell-commands.
+    """
     package_name = (
         Path("tests/pantstests/testdata/test_package_name.txt").read_text().strip()
     )
