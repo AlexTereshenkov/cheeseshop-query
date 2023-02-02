16a0d8247de9d94de17cffce7c037456288d6edc
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2023-02-02 20:11:16 +0000

    pants: add experimental_shell_command to use in a test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -15,7 +15,9 @@ def test_cli():
 @pytest.mark.vcr()
 def test_cli_info():
     runner = CliRunner()
-    result = runner.invoke(cli.cli, ["info", "--package", "packaging"])
+    with open("tests/testdata/test_package_name.txt") as fh:
+        package_name = fh.read().strip()
+    result = runner.invoke(cli.cli, ["info", "--package", package_name])
     assert result.exit_code == 0
     assert "Core utilities for Python packages" in result.output
 
---
+++ tests/write_test_package_name.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mkdir -p testdata
+echo "packaging" > testdata/test_package_name.txt
